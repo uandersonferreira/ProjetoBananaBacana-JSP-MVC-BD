ef6cf51511b4f59a042831ef2f22f2e3401b2bae
UandersonFerreira <103908858+UandersonFerreira@users.noreply.github.com>
2023-03-23 18:06:27 -0300

    commit test01

--- src/main/java/ClassTeste01.java
+++ src/main/java/ClassTeste01.java
@@ -1,3 +1,11 @@
 public class ClassTeste01 {
+    private int num1;
 
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
 }//class
